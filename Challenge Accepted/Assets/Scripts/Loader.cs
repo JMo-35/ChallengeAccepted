3537d23bfdf6f923dbdc107278469a6d1bcd00dc
JMo-35 <47646759+JMo-35@users.noreply.github.com>
2021-03-11 17:02:10 -0500

    xml update
    Added system to add in data for challenges. ADM user set to level 3 with a random task loaded.

---
+++ Challenge Accepted/Assets/Scripts/Loader.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+public class Loader : MonoBehaviour
+{
+
+    XDocument xmlDoc;
+    IEnumerable<XElement> items; 
+    List <XMLData> data = new List <XMLData>();
+    private int rank;
+    private string challenge;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject); 
+        LoadXML();
+        StartCoroutine("AssignData");
+    }
+
+    void LoadXML()
+    {
+        xmlDoc = XDocument.Load("Assets/XML_Data/challenge_accepted_data.xml");
+        items = xmlDoc.Descendants("record").Elements();
+    }
+
+    IEnumerator AssignData()
+    {
+        foreach (var item in items)
+        {
+            rank = int.Parse(item.Parent.Element("rank").Value);
+            challenge = item.Parent.Element("challenge").Value.Trim();
+            data.Add(new XMLData(rank, challenge));
+        }
+        yield return null;
+    }
+
+    //temporary function
+    public string GetRandomChallenge(int level)
+    {
+        if (level < 1 || level > 10)
+        {
+            return null;
+        }
+        List<string> tasks = new List<string>();
+        foreach (var task in data)
+        {
+            if (task.rankNum == level)
+            {
+                tasks.Add(task.challengeText);
+            }
+        }
+        string choice = tasks[Random.Range(0, tasks.Count)];
+        return choice;
+    }
+}
+
+public class XMLData
+{
+    public int rankNum;
+    public string challengeText;
+
+    public XMLData (int rank, string challenge)
+    {
+        rankNum = rank;
+        challengeText = challenge;
+    }
+}
---
+++ Challenge Accepted/Assets/Scripts/Temp_User.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Temp_User : MonoBehaviour
+{
+    public Text level, currentChallenge;
+    private int lvl = 3;
+    public Loader load;
+
+    private void Start()
+    {
+        level.text = lvl.ToString();
+        currentChallenge.text = load.GetRandomChallenge(lvl);
+    }
+}
